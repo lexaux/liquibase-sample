6ff9354abe937f5caf3c53400aa087c7ba1f19f6
Alexander Cherednichenko <lexaux@gmail.com>
2013-11-05 14:36:34 +0200

    Adding the MySQL connector, and initial changeset.

---
+++ liquibase.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SCRIPTDIR=`dirname $0`
+
+#####
+# Some configuration options:
+####
+DRIVER=com.mysql.jdbc.Driver
+CLASSPATH=$SCRIPTDIR/lib/mysql-connector-java-5.1.27-bin.jar
+CHANGELOG_FILE=$SCRIPTDIR/db/changelog.xml
+JDBC_URL="jdbc:mysql://localhost/lqsample"
+JDBC_USER="root"
+JDBC_PASSWORD="root"
+
+
+echo "changelog is $CHANGELOG_FILE"
+
+java -jar $SCRIPTDIR/lib/liquibase.jar \
+     --driver="$DRIVER" \
+     --classpath="$CLASSPATH" \
+     --changeLogFile="$CHANGELOG_FILE" \
+     --url="$JDBC_URL" \
+     --username="$JDBC_USER" \
+     --password="$JDBC_PASSWORD" \
+     migrate
