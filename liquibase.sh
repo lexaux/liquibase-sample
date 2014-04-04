55600b768f456b75f7d9e23e673bcb3ce414c0e0
Alexander Cherednichenko <lexaux@gmail.com>
2014-04-04 12:48:07 +0300

    Small fix for the file to support multiple databases (needed for branching tests

--- liquibase.sh
+++ liquibase.sh
@@ -5,10 +5,26 @@ SCRIPTDIR=`dirname $0`
 #####
 # Some configuration options:
 ####
+echo "Total number of params is $#"
+if [ 2 -ne $# ] 
+then
+   echo "Should have exactly 2 arguments!"
+   echo ""
+   echo "Usage:  ./liquibase.sh DBNAME COMMAND"
+   echo "    DBNAME name of the database to apply command to on localhost"
+   echo "    COMMAND liquibase command, like update, updateSQL, etc." 
+   exit 1
+fi
+
+DBNAME=$2
+COMMAND=$1	
+echo "Command is $COMMAND"
+echo "DB name is $DBNAME"
+
 DRIVER=com.mysql.jdbc.Driver
 CLASSPATH=$SCRIPTDIR/lib/mysql-connector-java-5.1.27-bin.jar
 CHANGELOG_FILE=$SCRIPTDIR/db/changelog.xml
-JDBC_URL="jdbc:mysql://localhost/lqsample"
+JDBC_URL="jdbc:mysql://localhost/$DBNAME"
 JDBC_USER="root"
 JDBC_PASSWORD="root"
 
@@ -22,4 +38,4 @@ java -jar $SCRIPTDIR/lib/liquibase.jar \
      --url="$JDBC_URL" \
      --username="$JDBC_USER" \
      --password="$JDBC_PASSWORD" \
-     $@
+     $COMMAND 
